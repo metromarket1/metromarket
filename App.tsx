8d1248a18cd4bf0183d6ea67b385b29b58b00258
Amro Othman <amro2003.tariq@gmail.com>
2025-12-10 16:23:45 +0200

    fix: Update react-router-dom to version 6.30.2 and adjust routing in App component

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
   );
